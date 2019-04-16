4fcc094a2281ff005ae01b489113f3d7d0b702a9
Mohammad Mahdi Shokri <mmahdishokri@gmail.com>
2019-04-16 19:16:34 +0430

    basic implementations

--- scanner.py
+++ scanner.py
@@ -1,66 +1,26 @@
-class _Getch:
-    """Gets a single character from standard input.  Does not echo to the screen."""
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
+import getchar
+from constant import EOF_STATE
 
-    def __call__(self): return self.impl()
+def get_next_state(state, c):
+    return state + 1
 
+def get_next_token(state, word_wrapper, tokens):
+    word = word_wrapper[0]
+    c = code.read(1)
+    word = word + c
+    next_state = get_next_state(state, c)
+    tokens.append(word)
+    return next_state
 
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
+print("Hello! I am your scanner ^_^")
 
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+code = open("code.c", "r")
+output = open("scanner.txt")
+errors = open("lexical_errors.txt")
 
+tokens = []
+state = 0
+word_wrapper = [""]
 
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        print("hereeee!")
-        c = msvcrt.getch()
-        print("c = " + c)
-        return c
-
-
-getch = _Getch()
-
-print("Hello world!")
-
-f = open("code.c", "r")
-
-keywords = {'if', 'else', 'void',  'int',  'while',  'break',  'continue',  'switch',  'default',  'case',  'return'}
-
-word = ""
-
-while True:
-    print("Here!")
-    c = f.read(1)
-    print('c = ' + c)
-    if c == '\n':
-        break
-    word += c
-print('word = ' + word)
-
-
-def get_next_token(word):
-    token = ""
-    word = word + getch()
-    for keyword in keywords:
-        if keyword == word:
-            token = keyword
-    return token
+while state != EOF_STATE:
+    state = get_next_token(state, word_wrapper, tokens)
